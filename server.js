0770bb6fce0f3ec0e1befd851b6a18a6541911d3
tnk976 <badrane976@gmail.com>
2018-06-20 10:34:04 +0400

    affichage des données de mongodb dans la console

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
+
+var url = 'mongodb://localhost:27017/badrane';
+
+MongoClient.connect(url, function(err, db){
+    assert.equal(null, err)
+    console.log("Connected successfully to server");
+    var dbo = db.db("badrane");
+    dbo.collection("personnages").find({}).toArray(function(err, data){
+        if (err) throw err;
+        console.log(data);
+        db.close();
+      }); 
+});
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
